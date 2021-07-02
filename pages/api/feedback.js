3d9cddcf830f2ae518b417011244cc4b95e38a21
ms77grz <ms77grz@gmail.com>
2021-07-02 18:00:32 +0300

    Parsing The Incoming Request & Executing Server-side Code - 45%

---
+++ pages/api/feedback.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text: feedbackText,
+    };
+  }
+
+  res.status(200).json({ message: 'This works!' });
+}
